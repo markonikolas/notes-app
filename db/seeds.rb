c953f8191b17e106a0083a29858899d7f7c7d1df
Marko Nikolaš <marko.nikolas@protonmail.com>
2021-06-02 10:17:55 +0200

    Generate schema and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+notes = Note.create([
+	                    {title: 'Welcome to Rails!', body: 'Hello Rails!!!'},
+	                    {title: 'Test Note', body: 'Hello World!!!'}
+                    ])
