9857ed3384737b60061d9edf06fb73b25d2e34e8
Marko Nikolaš <marko.nikolas@protonmail.com>
2021-06-02 13:24:19 +0200

    Update show and index template, add show action and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
 	root 'notes#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	get 'notes/:id', to: 'notes#show', as: :note_show
 end
