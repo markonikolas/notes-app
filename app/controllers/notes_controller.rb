2cf95a43dd23727605d9d7465a05047c5994cb2d
Marko Nikolaš <marko.nikolas@protonmail.com>
2021-06-03 13:32:28 +0200

    Update routes, and fix functionality for edit and new action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,7 +2,7 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
-	
+  
   def show
     @note = Note.find(params[:id])
   end
@@ -10,7 +10,21 @@ class NotesController < ApplicationController
   def edit
     @note = Note.find(params[:id])
   end
-  
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    
+    if @note.save
+      redirect_to note_path(@note)
+    else
+      render :new
+    end
+  end
+
   def update
     @note = Note.find(params[:id])
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-	root 'notes#index'
-	get 'notes/:id', to: 'notes#show', as: :note
-	get 'notes/edit/:id', to: 'notes#edit', as: :note_edit
-	
-	get 'notes/update/:id', to: 'notes#show'
-	patch 'notes/:id', to: 'notes#update'
+	resources :notes
+	# get 'notes/new', to: 'notes#new', as: :note_new
+	# get 'notes/:id', to: 'notes#show', as: :notes
+	# get 'notes/edit/:id', to: 'notes#edit', as: :note_edit
+	#
+	# get 'notes/update/:id', to: 'notes#show'
+	# patch 'notes/:id', to: 'notes#update'
 end
