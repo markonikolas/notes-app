9c0a958a91ac3657ac39c1bf450b6fe3140782ad
Marko Nikolaš <marko.nikolas@protonmail.com>
2021-06-03 13:35:41 +0200

    Update code style and remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
-# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
 	resources :notes
-	# get 'notes/new', to: 'notes#new', as: :note_new
-	# get 'notes/:id', to: 'notes#show', as: :notes
-	# get 'notes/edit/:id', to: 'notes#edit', as: :note_edit
-	#
-	# get 'notes/update/:id', to: 'notes#show'
-	# patch 'notes/:id', to: 'notes#update'
 end
